dba5c8f75f7d777f93f007b1dc0917a79c13867b
mo <chatmoon@gmail.com>
2019-03-06 16:27:36 +0100

    HF10_mo: curly brace

--- Gameboard.cpp
+++ Gameboard.cpp
@@ -26,8 +26,8 @@ Gameboard::Gameboard() { //define the constructor
             //test1: OK - diag1 - if(i==(3-j)) { gameSpace[i][j] = "x"; }
             //test2: OK - row - gameSpace[1][j] = "x";
             //test3: OK - col - gameSpace[i][1] = "x";
-        };
-    };
+        }
+    }
 }
 
 //Gameboard::~Gameboard() { cout<<"\n  Deleting the Gameboard object\n\n"; } //define the destructor
@@ -39,9 +39,9 @@ void Gameboard::printInfo() { //print the game board in a 4x4 matrix
     for(int i=0;i<4;i++) {
         for(int j=0;j<4;j++) {
             cout<<gameSpace[i][j]<<"\t";
-        };
+        }
         cout<<"\n";
-    };
+    }
 }
 int Gameboard::findFour(char chr) { //four symbols in any row, col, diagonals 'wins'
     int int_dg0=0, int_dg1=0;
@@ -52,9 +52,9 @@ int Gameboard::findFour(char chr) { //four symbols in any row, col, diagonals 'w
             if(gameSpace[j][i][0]==chr) { ++int_col;}
             if( (gameSpace[i][j][0]==chr)&&(i==j) ) { ++int_dg0;}
             if( (gameSpace[i][j][0]==chr)&&(i==(3-j)) ) { ++int_dg1;}
-        };
+        }
         if((int_row==4)||(int_col==4)||(int_dg0==4)||(int_dg1==4)) { return 1; }
-    };
+    }
     return 0;
 }
 
