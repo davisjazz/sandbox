eaf00747e814da196cd63a6aa4e88a968050b553
mojazz <davisjazz@gmail.com>
2019-03-07 10:56:16 +0100

    [User] split in {.cpp: implementation, .h: interface}, replace using..std by std::, remove count data attributes

--- User.cpp
+++ User.cpp
@@ -3,33 +3,17 @@
 
 /* ----------- header file ------------------------------------------ */
 #include "main.hpp"
+#include "User.h"
 using namespace std;
 /* ----------- class User ------------------------------------------- */
-class User {                             //create a CLASS (type) called 'User'
-    string name;                             //data attribute - private
-    char   chr;
-    int    count;
-    public:                                  //procedural attribute- public
-        User();                                  //constructors
-        void set_name  (string in_name);         //mutator method : SETTER
-        void set_symbol(char   in_chr);
-        void set_count (int    in_count);
-        string get_name();                   //accessor method: GETTER
-        char   get_symbol();
-        int    get_count();
-        void  printInfo();                       //helper functions
-        //~User();                                 //destructors
-};
-User::User()  { name="Unkonw"; chr='.'; count=0; }          //define the constructor
-//User::~User() { cout<<"\n  > Deleting the User object\n\n"; } //define the destructor
+User::User()  { name="Unkonw"; chr='.'; }          //define the constructor
 
-void User::set_name(string in_name) { name  = in_name; }    //mutator method
-void User::set_symbol (char in_chr) { chr   = in_chr; }
-void User::set_count (int in_count) { count = in_count; }
-string User::get_name()   { return name; }                  //accessor method
-char   User::get_symbol() { return chr; }
-int    User::get_count()  { return count; }
-void   User::printInfo() { cout<<name<<"\t"<<chr<<"\t"<<count; }
+void User::set_name(std::string in_name) { name  = in_name; }    //mutator method
+void User::set_symbol (char in_chr)      { chr   = in_chr; }
+
+std::string User::get_name() { return name; }                  //accessor method
+char   User::get_symbol()    { return chr; }
+void   User::printInfo()     { cout<<name<<"\t"<<chr; }
 
 /* ----------- use class User --------------------------------------- */
 struct usrs {
@@ -39,7 +23,7 @@ struct usrs {
 usrs create_2user() {
     User array_usr[2];
     char array_chr[] = { 'x', 'o' };
-    string str0;
+    std::string str0;
 
     for(int i=0;i<2;i++) {
         cout<<"Enter player "<<i<<"'s name: ";
---
+++ User.h
@@ -0,0 +1,27 @@
+/*
+ * User.h
+ *
+ *  Created on: 07.03.2019
+ *      Author: mo
+ */
+
+#ifndef USER_H_
+#define USER_H_
+#include <iostream>
+#include <string>
+
+class User {                             //create a CLASS (type) called 'User'
+    std::string name;                             //data attribute - private
+    char   chr;
+    int    count;
+    public:                                  //procedural attribute- public
+        User();                                  //constructors
+        void set_name  (std::string in_name);         //mutator method : SETTER
+        void set_symbol(char   in_chr);
+        std::string get_name();                   //accessor method: GETTER
+        char   get_symbol();
+        void  printInfo();                       //helper functions
+};
+
+
+#endif /* USER_H_ */
